476a9d12b77ee5026ffb09308816e8df4e7cfa82
hanhngooo <hanhngo1795@gmail.com>
2020-07-17 01:25:47 +0200

    Add date for post and comment

--- src/pages/PostDetail/index.js
+++ src/pages/PostDetail/index.js
@@ -4,6 +4,7 @@ import { useHistory, useParams, Link } from "react-router-dom";
 import { Image, CloudinaryContext } from "cloudinary-react";
 import { Row, Col, Container, Modal, Button } from "react-bootstrap";
 import "./style.css";
+import moment from "moment";
 import { AiOutlineClose } from "react-icons/ai";
 import { MdComment } from "react-icons/md";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
@@ -92,7 +93,7 @@ export default function PostDetail() {
           </Link>
           <Container className="modal-container">
             <Row>
-              <Col xs={12} md={8}>
+              <Col xs={10} md={8}>
                 {post.images &&
                   post.images.map((image) => {
                     return (
@@ -107,16 +108,19 @@ export default function PostDetail() {
                     );
                   })}
               </Col>
-              <Col xs={6} md={4}>
-                <Container className="post-detail">
+              <Col xs={6} md={4} className="post-detail">
+                <Container>
                   <Row className="user-name-card">
                     <Col xs={2}>
                       <MiniProfilePic
                         profile_pic={post.user && post.user.profile_pic}
                       />
                     </Col>{" "}
-                    <Col style={{ paddingTop: "0.5rem" }}>
+                    <Col>
                       <strong>{post.user && post.user.name}</strong>
+                      <div className="post-date">
+                        <i>{moment(post.createdAt).format("MMMM Do YYYY")}</i>
+                      </div>
                     </Col>
                   </Row>
                   <Row className="caption-card">
@@ -131,38 +135,41 @@ export default function PostDetail() {
                       <MdComment /> {post.comments_num} comments
                     </Col>
                   </Row>
-
-                  {post.comments &&
-                    post.comments.map((comment) => {
-                      return (
-                        <Container
-                          className="comment-container"
-                          key={comment.id}
-                        >
-                          <Row>
-                            <Col xs={2}>
-                              <MiniProfilePic
-                                profile_pic={
-                                  comment.user && comment.user.profile_pic
-                                }
-                              />
-                            </Col>
-                            <Col style={{ paddingTop: "0.5rem" }}>
-                              <strong>
-                                {comment.user && comment.user.name}
-                              </strong>
-                              {"  "}
-                              {comment.content}
-                            </Col>
-                          </Row>
-                        </Container>
-                      );
-                    })}
+                  <Row className="comment-all">
+                    {post.comments &&
+                      post.comments.map((comment) => {
+                        return (
+                          <Container
+                            className="comment-container"
+                            key={comment.id}
+                          >
+                            <Row>
+                              <Col xs={2}>
+                                <MiniProfilePic
+                                  profile_pic={
+                                    comment.user && comment.user.profile_pic
+                                  }
+                                />
+                              </Col>
+                              <Col className="comment-user">
+                                <strong>
+                                  {comment.user && comment.user.name}
+                                </strong>
+                              </Col>
+                              <Col className="comment-date">
+                                {moment(post.createdAt).format("MMMM Do YYYY")}
+                              </Col>
+                            </Row>
+                            <Row className="comment-content">
+                              <Col>{comment.content}</Col>
+                            </Row>
+                          </Container>
+                        );
+                      })}
+                  </Row>
                 </Container>
                 <Row className="comment-form">
-                  <Col xs={14}>
-                    <CommentForm postId={post.id} />
-                  </Col>
+                  <CommentForm postId={post.id} />
                 </Row>
               </Col>
             </Row>
