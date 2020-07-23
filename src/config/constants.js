136c0d84eb6542ede0799c9c4263cc086ec5d25e
hanhngooo <hanhngo1795@gmail.com>
2020-07-23 18:13:15 +0200

    Modify server url with heroku url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-export const apiUrl =
-  process.env.API_URL || "https://petseverywhere-svr.herokuapp.com/";
+export const apiUrl = "https://petseverywhere-svr.herokuapp.com/";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
